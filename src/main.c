2a6182d80bf60de8b238cc4df5a3b473c9ababc5
Okan Emre KOCA <oknakoca@gmail.com>
2024-05-21 12:24:10 +0200

    doing a flag array

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:24:39 by okoca             #+#    #+#             */
-/*   Updated: 2024/05/20 22:08:45 by okoca            ###   ########.fr       */
+/*   Updated: 2024/05/21 11:39:16 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,19 @@
 
 int	ft_printf(const char *str, ...)
 {
-	va_list args;
+	va_list	args;
+	size_t	counter;
+	size_t	number_of_args;
+	void	*current_arg;
 
+	counter = 0;
+	number_of_args = args_count(str);
 	va_start(args, str);
-
+	while (counter < number_of_args)
+	{
+		current_arg = va_arg(args, )
+		counter++;
+	}
 	printf("hello world, %s\n", str);
 
 	va_end(args);
--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 15:04:58 by okoca             #+#    #+#             */
-/*   Updated: 2024/05/20 22:08:34 by okoca            ###   ########.fr       */
+/*   Updated: 2024/05/21 12:21:54 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 #include <stdio.h>
 
-int	is_set(char c)
+int	is_set(char c, char *set)
 {
-	char	*set;
 	size_t	i;
 
 	i = 0;
-	set = ft_strdup("cspdiuxX%");
 	while (set[i])
 	{
 		if (c == set[i])
@@ -31,27 +29,32 @@ int	is_set(char c)
 	return (0);
 }
 
-size_t	args_count(const char*str)
+size_t	*args_count(const char*str)
 {
 	size_t	i;
 	size_t	count;
+	size_t	*flags;
+	char	*set;
 
 	i = 0;
 	count = 0;
+	set = ft_strdup("cspdiuxX%");
+	flags = (char *)malloc(sizeof(char) * (ft_strlen(set) + 1));
 	while (str[i])
 	{
-		if ((str[i] == '%' && is_set(str[i + 1])))
+		if ((str[i] == '%' && is_set(str[i + 1]), set))
 		{
 			if (str[i - 1] == '%' && str[i] == '%' && i != 0)
 				i++;
+			flags[count] = ft_strchr(set, str[i + 1]);
 			count++;
 		}
 		i++;
 	}
-	return (count);
+	flags[count] = NULL;
+	return (flags);
 }
 
 // char	**parse(const char *str)
 // {
-
 // }
